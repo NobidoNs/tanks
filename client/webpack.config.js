c6cd46d539cfaddccd7b126b4551aeca4898dec3
nobidons <nobidon.tim@gmail.com>
2025-08-21 11:44:06 +0500

    fix logic

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,79 +1,48 @@
-const path = require('path'),
-    HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const MASTER_JS_FILE = './src/client.js',
-    MASTER_HTML_FILE = './src/index.html',
-    OUTPUT_FOLDER = 'public',
-    OUTPUT_MASTER_JS = '[name].bundle.js';
+const path = require('path')
 
-module.exports = (env = {}, argv = {}) => {
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack')
 
-    return {
-        mode: argv.mode || env.mode || 'none',
-
-        entry: {
-            main: MASTER_JS_FILE
-        },
-
-        output: {
-            filename: OUTPUT_MASTER_JS,
-            path: path.resolve(__dirname, OUTPUT_FOLDER)
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.m?js$/,
-                    exclude: /(node_modules|bower_components)/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env']
-                        }
-                    }
-                }, {
-                    test: /\.css$/,
-                    use: ['style-loader', 'css-loader']
-                }, {
-                    test: /\.less$/,
-                    use: ['style-loader', 'css-loader', 'less-loader']
-                }, {
-                    test: /\.(jpe?g|png|gif|ico|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-                    use: 'base64-inline-loader?limit=1000&name=[name].[ext]'
-                }
-            ]
-        },
-
-        devtool: (argv.mode || env.mode) !== 'production'
-            ? 'inline-source-map'
-            : false,
-
-        devServer: {
-            static: {
-                directory: path.resolve(__dirname, OUTPUT_FOLDER)
-            },
-            compress: true,
-            port: 8000,
-            hot: true
-        },
-
-        plugins: getPlugins(env)
-    }
-}
-
-function getHtmlWebpackPluginOpt(env) {
-    const opt = {
-        template: MASTER_HTML_FILE,
-        minify: {
-            collapseInlineTagWhitespace: true,
-            collapseWhitespace: true
-        }
-    }
-    return opt;
-}
-
-function getPlugins(env) {
-    const plugins = [
-        new HtmlWebpackPlugin(getHtmlWebpackPluginOpt(env)),
-    ];
-    return plugins;
+module.exports = {
+  entry: './src/client.js',
+  output: {
+    filename: 'client.bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          'css-loader',
+          'less-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      jquery: require.resolve('jquery'),
+      lib: path.resolve(__dirname, 'lib'),
+      less: path.resolve(__dirname, './less')
+    },
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, './node_modules')
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].bundle.css',
+      chunkFilename: '[id].css'
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    })
+  ],
+  devtool: 'eval-cheap-source-map',
+  mode: 'development'
 }
