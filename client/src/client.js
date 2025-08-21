c9f2ba5f6f7c95779b977d5e2c4fc7a05f2a59bf
nobidons <nobidon.tim@gmail.com>
2025-08-21 11:14:21 +0500

    next socket fix

--- client/src/client.js
+++ client/src/client.js
@@ -1,7 +1,7 @@
 require('../less/styles.less')
 
 const $ = require('jquery')
-const { io } = require('../socket.io-client')
+const { io } = require('socket.io-client')
 
 const Chat = require('./game/Chat')
 const Game = require('./game/Game')
